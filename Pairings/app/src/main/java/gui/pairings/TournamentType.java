06c4dd724d2a35ab1520fcc425b563212e1ffc38
Cullen Rombach <cullenrombach@gmail.com>
2016-04-24 14:23:32 -0400

    Add base GUI activities They're empty, but they're there.

---
+++ Pairings/app/src/main/java/gui/pairings/TournamentType.java
@@ -0,0 +1,13 @@
+package gui.pairings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TournamentType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tournament_type);
+    }
+}
