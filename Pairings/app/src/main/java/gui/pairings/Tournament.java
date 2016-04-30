6209563cc96d2248b873e9aec6e14cc2c64b9113
rfbonett <rfbonett@email.wm.edu>
2016-04-29 22:46:06 -0400

    Attempt

--- Pairings/app/src/main/java/gui/pairings/Tournament.java
+++ Pairings/app/src/main/java/gui/pairings/Tournament.java
@@ -1,5 +1,7 @@
 package gui.pairings;
 
+import java.util.ArrayList;
+
 /**
  * Class that stores tournament information. Created for the purpose of showing dummy
  * tournaments on the home screen, but could be expanded later to be actually functional.
@@ -7,11 +9,16 @@ package gui.pairings;
 public class Tournament {
 
     private String name, type;
+    private ArrayList<Bracket> series;
+    private ArrayList<Player> players;
 
     // Constructor that takes the tournament name and type as a parameter.
     public Tournament(String name, String type) {
         this.name = name;
         this.type = type;
+        players = new ArrayList<>();
+        series = new ArrayList<>();
+        series.add(new Bracket());
     }
 
     // Set the name of this tournament.
@@ -28,4 +35,19 @@ public class Tournament {
     public String getType() {
         return type;
     }
+
+    public int numSeries() {
+        return 3;
+    }
+
+    public Bracket getBracket(int position) {
+        return series.get(0);
+    }
+
+    public void addPlayer(Player p) {
+        players.add(p);
+        series.get(0).addPlayer(p);
+    }
+
+
 }
--- Pairings/app/src/main/java/gui/pairings/TournamentScreen.java
+++ Pairings/app/src/main/java/gui/pairings/TournamentScreen.java
@@ -1,165 +1,225 @@
 package gui.pairings;
 
-import android.os.Bundle;
-import android.support.design.widget.TabLayout;
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class TournamentScreen extends AppCompatActivity {
-    private int round;
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v13.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    private ViewPager bracketPager;
+    private static Tournament tournament;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tournament_screen);
 
-        round = getIntent().getExtras().getInt("round");
-        String tournamentType = getIntent().getExtras().getString("tournamentType");
-        String tournamentName = getIntent().getExtras().getString("tournamentName");
         ArrayList<Player> players = (ArrayList<Player>) getIntent().getSerializableExtra("players");
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setTitle(tournamentName);
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        mSectionsPagerAdapter.setCount(round + 1);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        // Set up tabs
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        // Makes tabs with count from mViewPager and names from mSectionsPagerAdapter getPageTitle()
-        //tabLayout.setTabsFromPagerAdapter(mSectionsPagerAdapter);
-        tabLayout.setupWithViewPager(mViewPager);
+        String type = getIntent().getStringExtra("tournamenttype");
+        String name = getIntent().getStringExtra("tournamentname");
+        tournament = new Tournament(name, type);
+        if (players != null) {
+            for (Player p : players) {
+                tournament.addPlayer(p);
+            }
+        }
+        bracketPager = (ViewPager) findViewById(R.id.bracketPager);
+        bracketPager.setAdapter(new BracketAdapter(getSupportFragmentManager()));
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_tournament_screen, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_save :
+                saveTournament();
+                break;
+            case R.id.action_share :
+                shareTournament();
+                break;
+            case R.id.action_back :
+                saveTournament();
+                returnToMain();
+                break;
+            case R.id.action_settings :
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_tournament_screen, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-        }
+    public void saveTournament() {
+        Toast.makeText(TournamentScreen.this, "Tournament Saved!", Toast.LENGTH_SHORT).show();
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    public void shareTournament() {
+        final Dialog prompt = new Dialog(this);
+        prompt.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        prompt.setContentView(R.layout.share_dialog);
+        //prompt.setTitle("Share " + (tournament.getName() != null ? tournament.getName() : "Tournament"));
+        prompt.show();
+    }
+
+    public void returnToMain() {
+        Intent i = new Intent(this, HomeScreen.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+    }
+
+    public void shareFacebook(View v) {
+        Toast.makeText(TournamentScreen.this, "Would launch Facebook!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void shareTwitter(View v) {
+        Toast.makeText(TournamentScreen.this, "Would launch Twitter!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void shareGmail(View v) {
+        Toast.makeText(TournamentScreen.this, "Would launch Gmail!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    class BracketAdapter extends FragmentStatePagerAdapter {
+
         private int count;
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
+        public BracketAdapter(FragmentManager manager) {
+            super(manager);
+            count = tournament.numSeries();
         }
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            BracketFragment bracket = new BracketFragment();
+            Bundle args = new Bundle();
+            args.putInt(BracketFragment.ARG_OBJECT, position);
+            bracket.setArguments(args);
+            return bracket;
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
             return count;
         }
+    }
+
+    public static class BracketFragment extends Fragment {
+        public static final String ARG_OBJECT = "bracket";
+        private Bracket bracket;
+
 
         @Override
-        public CharSequence getPageTitle(int position) {
-            if (position <= count) {
-                return "ROUND " + (position + 1);
-            }
-            return null;
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.bracket_layout, container, false);
+            int pos = getArguments().getInt(ARG_OBJECT);
+            bracket = tournament.getBracket(pos);
+            ListView l = (ListView) rootView.findViewById(R.id.bracketListView);
+            l.setAdapter(new ChallengeListAdapter(getActivity(), bracket.getChallenges()));
+
+            return rootView;
         }
 
-        public void setCount(int newCount) {
-            count = newCount;
+
+        class ChallengeListAdapter extends BaseAdapter {
+            private ArrayList<Challenge> challenges;
+            private final Context context;
+
+            public ChallengeListAdapter(Context context, ArrayList<Challenge> challenges) {
+                this.context = context;
+                this.challenges = challenges;
+            }
+
+            @Override
+            public int getCount() {
+                return challenges.size();
+            }
+
+            @Override
+            public Object getItem(int i) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                LayoutInflater inflater = LayoutInflater.from(context);
+                View bracketView = inflater.inflate(R.layout.challenge, viewGroup, false);
+                final Challenge c = challenges.get(i);
+                int width = (int) getResources().getDimension(R.dimen.button_width);
+                int height = (int) getResources().getDimension(R.dimen.button_height);
+                if (c.getFirst() != null) {
+                    Button first = new Button(context);
+                    first.setText(c.getFirst().getName());
+                    first.setLayoutParams(new FrameLayout.LayoutParams(width, height));
+                    first.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            launchResultDialog(c);
+                        }
+                    });
+                    ((FrameLayout) bracketView.findViewById(R.id.firstPlayer)).addView(first);
+                }
+                if (c.getSecond() != null) {
+                    Button second = new Button(context);
+                    second.setText(c.getSecond().getName());
+                    second.setLayoutParams(new FrameLayout.LayoutParams(width, height));
+                    second.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            launchResultDialog(c);
+                        }
+                    });
+                    ((FrameLayout) bracketView.findViewById(R.id.secondPlayer)).addView(second);
+                }
+                return bracketView;
+            }
+
+            public void launchResultDialog(Challenge c) {
+                final Dialog d = new Dialog(context);
+                d.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                d.setContentView(R.layout.result_dialog);
+                TextView first = (TextView) d.findViewById(R.id.firstName);
+                first.setText(c.getFirst().getName());
+                TextView second = (TextView) d.findViewById(R.id.secondName);
+                second.setText(c.getSecond().getName());
+                d.show();
+            }
         }
     }
+
+
+
 }
